b6bae096a8b13d17630249ee58cc5d7c0ff94482
Rakibul Hasan <rakibulhasan745101@gmail.com>
2024-01-01 01:27:29 +0600

    express.urlencoded deleted

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, async () => {
